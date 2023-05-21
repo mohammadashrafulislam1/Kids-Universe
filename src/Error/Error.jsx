76165a6a60ec7a0324b4ac18962e60fabe1b17de
mohammadashrafulislam <ixdell41@gmail.com>
2023-05-21 17:35:48 +0600

    some basic design

--- src/Error/Error.jsx
+++ src/Error/Error.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 const Error = () => {
     return (
-        <div className="w-1/2 mx-auto">
+        <div className="w-1/2 mx-auto relative">
         <img src="https://cdn.pixabay.com/photo/2021/07/21/12/49/error-6482984_1280.png" alt="" />
+        <Link to='/'>
+        <button className="btn absolute top-1/2 right-1/3">Back To Home</button></Link>
         </div>
     );
 };
